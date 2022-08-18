4ca80edd1da6a4268f676b055b4357ac41523c9a
AS <ascii2017r@gmail.com>
2022-08-18 19:22:41 +0200

    asking

--- 1 - Bash Scripting/Useful snippets/LOOPS/Infinite loop.sh
+++ 1 - Bash Scripting/Useful snippets/LOOPS/Infinite loop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 for (( ; ; ));
-#Remember that the spacing in this 'for' are mandatory! 
+#Remember that the spacing in this 'for' are mandatory!
 #if you type 'for ((;;));' it will not work
 do
 echo -n "Are you ok? "
@@ -10,7 +10,10 @@ read -a answer
         if [ $answer == 'y' ];then
                 echo "Ok! I'm glad that you are fine. Bye!"
                 break
-
+        else
+                echo "oh. I'm sorry ! bye!"
+                break
+                #or you can close your code with 'exit 1'
         fi
 
 
